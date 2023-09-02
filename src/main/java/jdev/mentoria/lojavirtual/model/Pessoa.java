fd165e4d1f9841f03b3ba7d5ecbce68b3c03fd6d
Lenovo <Lenovo@LAPTOP-IN00I31T>
2023-09-02 13:36:40 -0300

    implementacao classe endereco

--- src/main/java/jdev/mentoria/lojavirtual/model/Pessoa.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/Pessoa.java
@@ -1,14 +1,19 @@
 package jdev.mentoria.lojavirtual.model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.SequenceGenerator;
 
 @Entity
@@ -33,6 +38,17 @@ public abstract class Pessoa implements Serializable {
 	
 	private String telefone;
 
+	
+	@OneToMany(mappedBy = "pessoa", orphanRemoval = true, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<Endereco> endereco = new ArrayList<>();
+	
+	public void setEndereco(List<Endereco> endereco) {
+		this.endereco = endereco;
+	}
+	
+	public List<Endereco> getEndereco() {
+		return endereco;
+	}
 
 	public Long getId() {
 		return id;
