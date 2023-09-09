ad81f0ace48e0037937031aaaec0d612b3bcba4b
bapazi81_40n <svecontabilidade@gmail.com>
2023-09-09 08:25:28 -0300

    aula mentoria 09092023

--- src/main/java/jdev/mentoria/lojavirtual/model/VendaCompraLojaVirtual.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/VendaCompraLojaVirtual.java
@@ -5,6 +5,7 @@ import java.math.BigDecimal;
 import java.util.Date;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ForeignKey;
@@ -50,6 +51,7 @@ public class VendaCompraLojaVirtual implements Serializable{
 	foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "endereco_cobranca_fk"))
 	private Endereco enderecoCobranca;
 	
+	@Column(nullable = false)
 	private BigDecimal valorTotal;
 	
 	private BigDecimal valorDesconto;
@@ -65,17 +67,20 @@ public class VendaCompraLojaVirtual implements Serializable{
 	private NotaFiscalVenda notaFiscalVenda;
 	
 	@ManyToOne
-	@JoinColumn(name = "cupom_desc_id", nullable = false,
+	@JoinColumn(name = "cupom_desc_id",
 	foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "cupom_desc_fk"))
 	private CupDesconto cupDesconto;
 	
+	@Column(nullable = false)
 	private BigDecimal valorFrete;
 	
+	@Column(nullable = false)
 	private Integer diaEntrega;
 	
+	@Column(nullable = false)
 	@Temporal(TemporalType.DATE)
 	private Date dataVenda;
-	
+
 	@Temporal(TemporalType.DATE)
 	private Date dataEntrega;
 
